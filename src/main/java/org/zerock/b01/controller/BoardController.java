16998df7fb3aab8e14060885a654639e184a1354
Yudain22 <di4046@gmail.com>
2024-05-14 11:22:45 +0900

    인증과 인가 권한

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -43,6 +44,7 @@ public class BoardController {
     model.addAttribute("responseDTO", responseDTO);
   }
 
+  @PreAuthorize("hasRole('USER')")
   @GetMapping("/register")
   public void registerGET(){}
 
