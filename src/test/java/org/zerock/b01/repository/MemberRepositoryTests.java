af78b286df6fea65777ac9f807318e6c00c83422
Yudain22 <di4046@gmail.com>
2024-05-17 09:44:20 +0900

    회원 조회 테스트

--- src/test/java/org/zerock/b01/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/b01/repository/MemberRepositoryTests.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.zerock.b01.domain.Member;
 import org.zerock.b01.domain.MemberRole;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -22,19 +23,46 @@ public class MemberRepositoryTests {
 
     @Test
     public void insertMembers(){
+        //IntStream 1~100까지 반복문을 위해 사용
         IntStream.rangeClosed(1,100).forEach(i->{
+            //회원 생성 1~100
             Member member = Member.builder()
                     .mid("member" + i )
+                    //CustomUserDetailsService에서 BCryptPasswordEncoder했기 때문에 passwordEncoder는 암호화 됨
+                    //encode 때문에 다 다르게 나옴
                     .mpw(passwordEncoder.encode("1111"))
                     .email("email"+i+"@aaa.bbb")
                     .build();
 
+            //일반 회원의 권한 설정
+            //멤버 유저 추가 1~100까지는 멤버 유저 권한
             member.addRole(MemberRole.USER);
 
+            //관리자 권한 설정
+            //90이상의 사람들은 ADMIN 권한 사용도 가능
+            //member_role_set을 보면 90이상은 0,1권한 모두를 가짐
             if (i >= 90){
                 member.addRole(MemberRole.ADMIN);
             }
+            //데이터베이스에 저장
+            //save 데이터가 없으면 insert 있으면 update
             memberRepository.save(member);
         });
+    }
+
+    @Test
+    public void testRead(){
+        //데이터 베이스에서 mid를 기준으로 데이터를 취득
+        Optional<Member> result = memberRepository.getWithRoles("member100");
+
+        //에러 확인
+        Member member = result.orElseThrow();
+
+        //전체 데이터 출력
+        log.info(member);
+        //role 데이터 출력
+        log.info(member.getRoleSet());
+
+        member.getRoleSet().forEach(memberRole -> log.info(memberRole.name()));
     }
 }
