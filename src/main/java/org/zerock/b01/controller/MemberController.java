c1137743720fae6e4cbecc4329a626b0cd492357
Yudain22 <di4046@gmail.com>
2024-05-17 10:52:31 +0900

    회원 가입을 위한 DTO처리

--- src/main/java/org/zerock/b01/controller/MemberController.java
+++ src/main/java/org/zerock/b01/controller/MemberController.java
@@ -1,11 +1,12 @@
 package org.zerock.b01.controller;
 
 
-import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.b01.dto.MemberJoinDTO;
 
 @Controller
 @RequestMapping("/member")
@@ -20,5 +21,20 @@ public class MemberController {
         if(logout != null){
             log.info("user logout...");
         }
+    }
+
+    @GetMapping("/join")
+    public void joinGET(){
+
+        log.info("join get........");
+    }
+
+    @PostMapping("/join")
+    public String joinPOST(MemberJoinDTO memberJoinDTO){
+
+        log.info("join post........");
+        log.info("memberJoinDTO:" +memberJoinDTO);
+
+        return "redirect:/board/list";
     }
 }
