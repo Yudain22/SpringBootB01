5b72e47ee959673e3a191e461a1780d1952d4616
Yudain22 <di4046@gmail.com>
2024-05-17 10:27:31 +0900

    회원 security DTO처리와 서비스 처리

--- src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
+++ src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package org.zerock.b01.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,27 +10,65 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.repository.MemberRepository;
+import org.zerock.b01.security.dto.MemberSecurityDTO;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+//    private PasswordEncoder passwordEncoder;
 
-    public CustomUserDetailsService(){
-        this.passwordEncoder =new BCryptPasswordEncoder();
-    }
+//    public CustomUserDetailsService(){
+//        this.passwordEncoder =new BCryptPasswordEncoder();
+//    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("loadUserByUsername : " + username);
 
-        UserDetails userDetails = User.builder()
-                .username(username)
-                .password(passwordEncoder.encode("1111"))
-                .authorities("ROLE_USER")
-                .build();
+        //데이터베이스에서 username으로 검색한 회원정보를 취득
+        Optional<Member> result = memberRepository.getWithRoles(username);
 
-        return userDetails;
+        //회원정보 유무를 확인하는 if문
+        if(result.isEmpty()){
+            //isEmpty 비어있으면 UsernameNotFoundException 예외처리 후 진행 안 됨
+            throw new UsernameNotFoundException("username not found...");
+        }
+
+        //데이터가 있으면 멤버의 데이터를 저장
+        Member member = result.get();
+
+        //Member객체를 MemberSecurityDTO객체로 변환
+        MemberSecurityDTO memberSecurityDTO =
+                new MemberSecurityDTO(
+                        member.getMid(),
+                        member.getMpw(),
+                        member.getEmail(),
+                        member.isDel(),
+                        false,
+                        member.getRoleSet()
+                                //저장된 memberRole은 USER,ADMIN 두가지 결과가 리스트로 저장됨
+                                .stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_"+ memberRole.name()))
+                                .collect(Collectors.toList())
+                );
+
+        log.info("memberSecurityDTO");
+        log.info(memberSecurityDTO);
+
+        return memberSecurityDTO;
+
+//        UserDetails userDetails = User.builder()
+//                .username(username)
+//                .password(passwordEncoder.encode("1111"))
+//                .authorities("ROLE_USER")
+//                .build();
+//
+//        return userDetails;
     }
 }
---
+++ src/main/java/org/zerock/b01/security/dto/MemberSecurityDTO.java
@@ -0,0 +1,37 @@
+package org.zerock.b01.security.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@ToString
+//extends는 MemberSecurityDTO가 User를 상속받고 있음을 의미
+public class MemberSecurityDTO extends User {
+    //멤버 변수 설정
+    private String mid;
+    private String mpw;
+    private String email;
+    private Boolean del;
+    private Boolean social;
+
+    //생성자 : MemberSecurityDTO 객체를 만들기 위해 생성
+    //생성자 안 만들면 기본 생성자 private Member = new member(); 형태의  기본생성자 생김
+    public MemberSecurityDTO(String username, String password, String email, boolean del, boolean social, Collection<? extends GrantedAuthority> authorities)
+    {
+        //super는 부모 클래스의 생성자를 부르는 것 여기서는 User의 생성자 부름
+        super(username, password, authorities);
+
+        //객체 안의 멤버 변수에 각각의 데이터 설정부분
+        this.mid = username;
+        this.mpw = password;
+        this.email = email;
+        this.del = del;
+        this.social = social;
+    }
+}
